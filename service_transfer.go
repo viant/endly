5af0e9d2366d08f51302143354e0df9d110733a9
adrianwit <adrianwit@gmail.com>
2017-09-18 09:07:18 -0700

    added test fot transfer

--- service_transfer.go
+++ service_transfer.go
@@ -6,15 +6,22 @@ import (
 	"github.com/viant/toolbox/storage"
 	_ "github.com/viant/toolbox/storage/scp"
 	"io"
+	"io/ioutil"
+	"strings"
+	"github.com/viant/toolbox"
 )
 
 const TransferServiceId = "transfer"
 
 type TransfersRequest struct {
-	Transfers []*TransferRequest
+	Transfers []*Transfer
 }
 
-type TransferRequest struct {
+type TransfersResponse struct {
+	Transfered []*TransferInfo
+}
+
+type Transfer struct {
 	Source   *Resource
 	Target   *Resource
 	Parsable bool
@@ -47,8 +54,25 @@ type transferService struct {
 	*AbstractService
 }
 
-func (s *transferService) run(context *Context, transfers ...*TransferRequest) ([]*TransferInfo, error) {
-	var result = make([]*TransferInfo, 0)
+func NewExpandedContentHandler(context *Context) func(reader io.Reader) (io.Reader, error) {
+	return func(reader io.Reader) (io.Reader, error) {
+		content, err := ioutil.ReadAll(reader)
+		if err != nil {
+			return nil, err
+		}
+		expanded := context.Expand(string(content))
+		if err != nil {
+			return nil, err
+		}
+		return strings.NewReader(toolbox.AsString(expanded)), nil
+	}
+}
+
+
+func (s *transferService) run(context *Context, transfers ...*Transfer) (*TransfersResponse, error) {
+	var result = &TransfersResponse{
+		Transfered:make([]*TransferInfo, 0),
+	}
 	sessionInfo := context.SessionInfo()
 	for _, transfer := range transfers {
 
@@ -70,10 +94,11 @@ func (s *transferService) run(context *Context, transfers ...*TransferRequest) (
 		}
 		var handler func(reader io.Reader) (io.Reader, error)
 		if transfer.Parsable {
-			handler = common.NewHandler(context.Context)
+			handler = NewExpandedContentHandler(context)
 		}
 		err = storage.Copy(sourceService, source.URL, targetService, target.URL, handler)
 		info := NewTransferInfo(context, source.URL, target.URL, err, transfer.Parsable)
+		result.Transfered = append(result.Transfered, info)
 		sessionInfo.Log(info)
 		if err != nil {
 			return result, err
@@ -91,12 +116,6 @@ func (s *transferService) Run(context *Context, request interface{}) *ServiceRes
 		if err != nil {
 			response.Error = fmt.Sprintf("Failed to tranfer resources: %v, %v", actualRequest.Transfers, err)
 		}
-	case *TransferRequest:
-		response.Response, err = s.run(context, actualRequest)
-		if err != nil {
-			response.Error = fmt.Sprintf("Failed to tranfer resources: %v, %v", actualRequest, err)
-		}
-
 	default:
 		response.Error = fmt.Sprintf("Unsupported request type: %T", request)
 	}
@@ -110,7 +129,7 @@ func (s *transferService) NewRequest(action string) (interface{}, error) {
 	switch action {
 	case "run":
 		return &TransfersRequest{
-			Transfers: make([]*TransferRequest, 0),
+			Transfers: make([]*Transfer, 0),
 		}, nil
 	}
 	return s.AbstractService.NewRequest(action)
--- service_transfer_test.go
+++ service_transfer_test.go
@@ -1,1 +1,85 @@
 package endly_test
+
+import (
+	"testing"
+	"github.com/viant/endly"
+	"github.com/viant/toolbox"
+	"github.com/stretchr/testify/assert"
+	"github.com/viant/endly/common"
+)
+
+type TestConfig struct {
+	Endpoint struct {
+		Hostname string
+		Port string
+	}
+}
+
+func TestNewTransferService(t *testing.T) {
+
+	manager := endly.NewManager()
+	service, err := manager.Service(endly.TransferServiceId)
+	if assert.Nil(t, err) {
+
+
+		context := manager.NewContext(toolbox.NewContext())
+
+		endpointMap := common.NewMap()
+		endpointMap.Put("host", "127.0.0.1")
+		endpointMap.Put("port", "8080")
+		var state = context.State()
+		state.Put("endpoint", endpointMap)
+
+		{
+			response := service.Run(context, &endly.TransfersRequest{
+				Transfers: []*endly.Transfer{
+					{
+						Source:   endly.NewFileResource("test/transfer/config.json"),
+						Target:   endly.NewFileResource("/tmp/transfered.json"),
+						Parsable: true,
+					},
+				},
+			})
+			assert.Equal(t, "", response.Error)
+			serviceResponse, ok := response.Response.(*endly.TransfersResponse)
+			assert.True(t, ok)
+			assert.Equal(t, 1, len(serviceResponse.Transfered))
+			assert.Equal(t, "file:///tmp/transfered.json", serviceResponse.Transfered[0].Target)
+
+			reader, _, err := toolbox.OpenReaderFromURL("file:///tmp/transfered.json")
+			assert.Nil(t, err)
+			config := &TestConfig{}
+			err = toolbox.NewJSONDecoderFactory().Create(reader).Decode(config)
+			assert.Nil(t, err)
+			assert.Equal(t, "127.0.0.1", config.Endpoint.Hostname)
+			assert.Equal(t, "8080", config.Endpoint.Port)
+		}
+		{
+			response := service.Run(context, &endly.TransfersRequest{
+				Transfers: []*endly.Transfer{
+					{
+						Source:   endly.NewFileResource("test/transfer/config.json"),
+						Target:   endly.NewFileResource("/tmp/transfered.json"),
+					},
+				},
+			})
+			assert.Equal(t, "", response.Error)
+			serviceResponse, ok := response.Response.(*endly.TransfersResponse)
+			assert.True(t, ok)
+			assert.Equal(t, 1, len(serviceResponse.Transfered))
+			assert.Equal(t, "file:///tmp/transfered.json", serviceResponse.Transfered[0].Target)
+
+			reader, _, err := toolbox.OpenReaderFromURL("file:///tmp/transfered.json")
+			assert.Nil(t, err)
+			config := &TestConfig{}
+			err = toolbox.NewJSONDecoderFactory().Create(reader).Decode(config)
+			assert.Nil(t, err)
+			assert.Equal(t, "$endpoint.host", config.Endpoint.Hostname)
+			assert.Equal(t, "$endpoint.port", config.Endpoint.Port)
+		}
+		err = toolbox.RemoveFileIfExist("/tmp/transfered.json")
+		assert.Nil(t, err)
+
+	}
+
+}
