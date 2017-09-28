3d3aada176aff5e5a936ee5337d0fd8f5cf616f6
adrianwit <adrianwit@gmail.com>
2017-09-27 23:20:47 -0700

    added udf support

--- udf_registry.go
+++ udf_registry.go
@@ -1,4 +1,6 @@
 package endly
 
+import "github.com/viant/endly/common"
+
 //UdfRegistry represents a udf registry
-var UdfRegistry = make(map[string]func(source interface{}) (interface{}, error))
+var UdfRegistry = make(map[string]func(source interface{}, state common.Map) (interface{}, error))
