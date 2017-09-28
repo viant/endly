554fe2067de19dc84ccf6f72756c837a070e3180
adrianwit <adrianwit@gmail.com>
2017-09-27 23:17:56 -0700

    added test for function key

--- context_test.go
+++ context_test.go
@@ -9,6 +9,6 @@ import (
 
 func TestNewDefaultState(t *testing.T) {
 	state := endly.NewDefaultState()
-	var expanded = endly.Expand(state, "home = ${env.HOME} ")
+	var expanded = endly.ExpandAsText(state, "home = ${env.HOME} ")
 	assert.False(t, strings.Contains(expanded, "${env.HOME}"))
 }
