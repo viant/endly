8a5a344f06f81e5ceddeadc949654e775586fa72
adrianwit <adrianwit@gmail.com>
2017-11-16 10:03:16 -0500

    added ws example

---
+++ example/ws/reporter/app/reporter.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"flag"
+	"github.com/viant/endly/example/ws/reporter"
+	"github.com/viant/toolbox/url"
+	"log"
+)
+
+var configURI = flag.String("config", "config/config.json", "path to json config  file")
+var port = flag.String("port", "8085", "service port")
+
+func main() {
+	flag.Parse()
+	config := &reporter.Config{}
+	configResource := url.NewResource(*configURI)
+	err := configResource.JSONDecode(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	service, err := reporter.NewService(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	server := reporter.NewServer(*port, service)
+	if err != nil {
+		log.Fatal(err)
+	}
+	server.Start()
+}
---
+++ example/ws/reporter/server.go
@@ -0,0 +1,49 @@
+package reporter
+
+import (
+	"fmt"
+	"github.com/viant/toolbox"
+	"net/http"
+	"log"
+)
+
+type Server struct {
+	port string
+	serviceRouter *toolbox.ServiceRouter
+}
+
+func (s *Server) Start()  {
+
+	http.HandleFunc("/v1/", func(writer http.ResponseWriter, reader *http.Request) {
+		err := s.serviceRouter.Route(writer, reader)
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+	fmt.Printf("Started test server on port %v\n", s.port)
+	log.Fatal(http.ListenAndServe(":"+s.port, nil))
+}
+
+func NewServer(port string, service Service) *Server {
+
+
+	serviceRouter := toolbox.NewServiceRouter(
+		toolbox.ServiceRouting{
+			HTTPMethod: "POST",
+			URI:        "/v1/reporter/register/",
+			Handler:    service.Register,
+			Parameters: []string{"request"},
+		},
+
+		toolbox.ServiceRouting{
+			HTTPMethod: "POST",
+			URI:        "/v1/reporter/run/",
+			Handler:    service.Run,
+			Parameters: []string{"request"},
+		},
+	)
+	return &Server{
+		port: port,
+		serviceRouter: serviceRouter,
+	}
+}
