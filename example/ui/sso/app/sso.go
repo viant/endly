fc39d8c74218f0d4c92959f76e072c65a43d5adc
Adrian Witas <adrianwitas@Adrians-MacBook-Pro.local>
2017-11-27 21:32:46 -0800

    Added ui example

---
+++ example/ui/sso/app/sso.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"github.com/viant/toolbox/url"
+	"github.com/viant/endly/example/ui/sso"
+)
+
+var configURI = flag.String("config", "config/config.json", "path to json config file")
+
+
+func main() {
+	flag.Parse()
+	config := &sso.Config{}
+	configResource := url.NewResource(*configURI)
+	err := configResource.JSONDecode(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	service, err := sso.NewService(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	server, err := sso.NewServer(config, service)
+	if err != nil {
+		log.Fatal(err)
+	}
+	server.Start()
+}
