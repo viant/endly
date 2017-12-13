1af632778e1d36bc823ea951189f999e26dfeb52
adrianwit <adrianwit@gmail.com>
2017-12-13 12:24:39 -0800

    added lifecycle workflow unit test

---
+++ payload_test.go
@@ -0,0 +1,20 @@
+package endly_test
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/viant/endly"
+)
+
+func Test_IsAsciiPrintable(t *testing.T) {
+
+	assert.False(t, endly.IsASCIIText(string([]byte{0x1, 0x3, 0x32})))
+	assert.True(t, endly.IsASCIIText("abc\n"))
+	assert.True(t, endly.IsASCIIText("abc\t"))
+	assert.True(t, endly.IsASCIIText("abc\r"))
+	assert.True(t, endly.IsASCIIText("<-abc->"))
+	assert.True(t, endly.IsASCIIText("'"))
+	assert.True(t, endly.IsASCIIText("\""))
+
+}
+
