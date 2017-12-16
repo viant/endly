f127237f5e664b5c9e849f05411a0f02c19fae40
adrianwit <adrianwit@gmail.com>
2017-12-16 00:15:46 -0800

    udpated readme

--- context_test.go
+++ context_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 	"github.com/viant/toolbox"
 	"fmt"
+	"github.com/viant/toolbox/url"
 )
 
 func TestNewDefaultState(t *testing.T) {
@@ -17,6 +18,9 @@ func TestNewDefaultState(t *testing.T) {
 		assert.False(t, strings.Contains(expanded, expr))
 		assert.True(t, len(expr) > 0)
 	}
+
+	var expanded = state.ExpandAsText("${tmpdir.subdir}")
+	assert.Contains(t, expanded, "/subdir")
 
 }
 
@@ -35,4 +39,22 @@ func TestContext_AsRequest(t *testing.T) {
 	_, err =context.AsRequest("nop", "abc", map[string]interface{}{})
 	assert.NotNil(t, err)
 
+}
+
+func TestContext_Expand_Resource(t *testing.T) {
+	manager := endly.NewManager()
+	context := manager.NewContext(toolbox.NewContext())
+
+	_, err := context.ExpandResource(nil)
+	assert.NotNil(t, err)
+	_, err = context.ExpandResource(&url.Resource{})
+	assert.NotNil(t, err)
+
+}
+
+func TestContext_TerminalSession(t *testing.T) {
+	manager := endly.NewManager()
+	context := manager.NewContext(toolbox.NewContext())
+	_, err := context.TerminalSession(url.NewResource("mem:///"))
+	assert.NotNil(t, err)
 }
