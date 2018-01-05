d7ac18c7ff8d1b8418e31d5629f4041e79a7efcb
adrianwit <adrianwit@gmail.com>
2018-01-05 14:53:16 -0800

    patched transformWithUDF

--- udf.go
+++ udf.go
@@ -1,10 +1,18 @@
 package endly
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/viant/toolbox/data"
+)
 
 func transformWithUDF(context *Context, udfName, source string, payload interface{}) (interface{}, error) {
 	var state = context.state
 	var udf, has = UdfRegistry[udfName]
+	if !has {
+		if candidate, ok := state[udfName]; ok {
+			udf, has = candidate.(func(source interface{}, state data.Map) (interface{}, error))
+		}
+	}
 	if !has {
 		return nil, fmt.Errorf("failed to lookup udf: %v for: %v", udfName, source)
 	}
