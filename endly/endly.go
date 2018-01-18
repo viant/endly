98fbf2ee962d34702f740de62db83b66d9ac84d1
adrianwit <adrianwit@gmail.com>
2018-01-18 11:20:03 -0800

    added bootstrap

--- endly/endly.go
+++ endly/endly.go
@@ -1,38 +1,7 @@
 package main
 
-import (
-	"flag"
-	_ "github.com/go-sql-driver/mysql"
-	_ "github.com/lib/pq"
-	_ "github.com/viant/asc"
-	_ "github.com/viant/bgc"
-	"github.com/viant/endly"
-	_ "github.com/viant/endly/static"
-	_ "github.com/viant/toolbox/storage/aws"
-	_ "github.com/viant/toolbox/storage/gs"
-	"log"
-	"time"
-	"os"
-)
-
-var workflow = flag.String("r", "run.json", "path to workflow run request json file")
+import "github.com/viant/endly/bootstrap"
 
 func main() {
-	flag.Parse()
-	runner := endly.NewCliRunner()
-	var arguments = make([]interface{}, 0)
-	if len(os.Args) > 1 {
-		for i := 1;i<len(os.Args);i++{
-			if os.Args[i] == "-r" {
-				i++
-				continue
-			}
-			arguments = append(arguments, os.Args[i])
-		}
-	}
-	err := runner.Run(*workflow, arguments...)
-	if err != nil {
-		log.Fatal(err)
-	}
-	time.Sleep(time.Second)
+	bootstrap.Bootstrap()
 }
