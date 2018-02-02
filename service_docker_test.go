c33608ccfe0172f43cf8fff2dade50822e8c4537
adrianwit <adrianwit@gmail.com>
2018-02-02 14:37:32 -0800

    enhance docker service start

--- service_docker_test.go
+++ service_docker_test.go
@@ -9,7 +9,10 @@ import (
 	"github.com/viant/toolbox/url"
 	"path"
 	"testing"
+	"strings"
 )
+
+
 
 func TestDockerService_Images(t *testing.T) {
 	var credentialFile, err = GetDummyCredential()
@@ -212,7 +215,7 @@ func TestDockerService_Run(t *testing.T) {
 			},
 			&endly.DockerContainerInfo{},
 			"testMysql01",
-			"failed to run container {\"Target\":{\"URL\":\"scp://127.0.0.1:22/\",\"Credential\":\"/var/folders/gl/5550g3kj6tn1rbz8chqx1c61ycmmm1/T/dummy11.json\",\"Cache\":\"\",\"CacheExpiryMs\":0,\"Name\":\"testMysql01\",\"Type\":\"\"},\"Image\":\"mysql:5.6\",\"Port\":\"\",\"Credentials\":{\"**mysql**\":\"/var/folders/gl/5550g3kj6tn1rbz8chqx1c61ycmmm1/T/mysql11.json\"},\"Env\":{\"MYSQL_ROOT_PASSWORD\":\"**mysql**\"},\"Mount\":{\"/tmp/my.cnf\":\"/etc/my.cnf\"},\"MappedPort\":{\"3306\":\"3306\"},\"Params\":null,\"Workdir\":\"\"}\n, error executing docker run --name testMysql01 -e MYSQL_ROOT_PASSWORD=**mysql** -v /tmp/my.cnf:/etc/my.cnf -p 3306:3306  -d mysql:5.6 , c3d9749a1dc43332bb5a58330187719d14c9c23cee55f583cb83bbb3bbb98a80\ndocker: Error response from daemon: driver failed programming external connectivity on endpoint testMysql01 (5c9925d698dfee79f14483fbc42a3837abfb482e30c70e53d830d3d9cfd6f0da): Error starting userland proxy: Bind for 0.0.0.0:3306 failed: port is already allocated.\n",
+			"error executing docker run --name testMysql01 -e MYSQL_ROOT_PASSWORD=**mysql** -v /tmp/my.cnf:/etc/my.cnf -p 3306:3306  -d mysql:5.6 , c3d9749a1dc43332bb5a58330187719d14c9c23cee55f583cb83bbb3bbb98a80\ndocker: Error response from daemon: driver failed programming external connectivity on endpoint testMysql01 (5c9925d698dfee79f14483fbc42a3837abfb482e30c70e53d830d3d9cfd6f0da): Error starting userland proxy: Bind for 0.0.0.0:3306 failed: port is already allocated.\n",
 		},
 		{
 			"test/docker/run/active/darwin",
@@ -256,7 +259,9 @@ func TestDockerService_Run(t *testing.T) {
 				serviceResponse := service.Run(context, useCase.Request)
 
 				var baseCase = useCase.baseDir + " " + useCase.TargetName
-				assert.Equal(t, useCase.Error, serviceResponse.Error, baseCase)
+
+				assert.True(t, strings.Contains(serviceResponse.Error, useCase.Error), baseCase)
+
 
 				actual, ok := serviceResponse.Response.(*endly.DockerContainerInfo)
 				if !ok {
