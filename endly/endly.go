732d7d35d0b175875e133e16bf69785bf3388d85
adrianwit <adrianwit@gmail.com>
2018-02-07 17:18:55 -0800

    added defulat import

--- endly/endly.go
+++ endly/endly.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
+	_ "github.com/viant/asc"
+	_ "github.com/viant/bgc"
+	_ "github.com/viant/endly/static" //load external resource like .csv .json files to mem storage
+	_ "github.com/viant/toolbox/storage/aws"
+	_ "github.com/viant/toolbox/storage/gs"
+
 	"github.com/viant/endly/bootstrap"
 )
 
