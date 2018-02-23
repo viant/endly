fb9c4f8e2d9e84da97eff32c4d1961ccd349a7e0
adrianwit <adrianwit@gmail.com>
2018-02-23 13:37:38 -0800

    updated examples

--- example/etl/transformer/app/transformer.go
+++ example/etl/transformer/app/transformer.go
@@ -7,19 +7,19 @@ import (
 	"flag"
 	"github.com/viant/endly/example/etl/transformer"
 	"log"
+	"github.com/viant/toolbox/url"
 )
 
 var configURI = flag.String("config", "", "path to json config file")
 
 func main() {
-	//flag.Parse()
-	config := &transformer.Config{Port:"8889"}
-
-	//configResource := url.NewResource(*configURI)
-	//err := configResource.JSONDecode(config)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
+	flag.Parse()
+	config := &transformer.Config{}
+	configResource := url.NewResource(*configURI)
+	err := configResource.JSONDecode(config)
+	if err != nil {
+		log.Fatal(err)
+	}
 	service := transformer.NewService()
 	server, err := transformer.NewServer(config, service)
 	if err != nil {
