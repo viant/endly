977dc6eb2616f01f66e7b4d926be55faf74fe97d
adrianwit <adrianwit@gmail.com>
2018-03-16 22:44:11 -0700

    added unit test

---
+++ system/docker/service_contract_test.go
@@ -0,0 +1,148 @@
+package docker
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/viant/assertly"
+	"github.com/viant/toolbox/url"
+	"testing"
+)
+
+func TestNewRequestFromURL(t *testing.T) {
+
+	var useCases = []struct {
+		Description string
+		URL         string
+		Provider    func(URL string) interface{}
+		Expected    interface{}
+	}{
+		{
+			Description: "ImagesRequest",
+			URL:         "test/req/images.yaml",
+			Provider: func(URL string) interface{} {
+				req, _ := NewImagesRequestFromURL(URL)
+				return req
+			},
+			Expected: `{
+  "Target": {
+    "URL": "ssh://127.0.0.1/",
+    "Credential": "localhost"
+  },
+  "Repository": "mysql",
+  "Tag": "5.6"
+}`,
+		},
+		{
+			Description: "RunRequest JSON",
+			URL:         "test/req/run.json",
+			Provider: func(URL string) interface{} {
+				req, _ := NewRunRequestFromURL(URL)
+				return req
+			},
+
+			Expected: `{
+  "Target": {
+    "URL": "scp://127.0.0.1:22/",
+    "Credential": "/var/folders/gl/5550g3kj6tn1rbz8chqx1c61ycmmm1/T/dummy20.json"
+  },
+  "Name": "testMysql",
+  "Secrets": {
+    "**mysql**": "/var/folders/gl/5550g3kj6tn1rbz8chqx1c61ycmmm1/T/mysql20.json"
+  },
+  "Image": "mysql:5.6",
+  "Port": "",
+  "Env": {
+    "MYSQL_ROOT_PASSWORD": "**mysql**"
+  },
+  "Mount": {
+    "/tmp/my.cnf": "/etc/my.cnf"
+  },
+  "Ports": {
+    "3306": "3306"
+  }
+}
+`,
+		},
+		{
+			Description: "RunRequest YAML",
+			URL:         "test/req/run.yaml",
+			Provider: func(URL string) interface{} {
+				req, _ := NewRunRequestFromURL(URL)
+				return req
+			},
+
+			Expected: `{
+  "Target": {
+    "URL": "scp://127.0.0.1:22/",
+    "Credential": "/var/folders/gl/5550g3kj6tn1rbz8chqx1c61ycmmm1/T/dummy20.json"
+  },
+  "Name": "testMysql",
+  "Secrets": {
+    "**mysql**": "/var/folders/gl/5550g3kj6tn1rbz8chqx1c61ycmmm1/T/mysql20.json"
+  },
+  "Image": "mysql:5.6",
+  "Port": "",
+  "Env": {
+    "MYSQL_ROOT_PASSWORD": "**mysql**"
+  },
+  "Mount": {
+    "/tmp/my.cnf": "/etc/my.cnf"
+  },
+  "Ports": {
+    "3306": "3306"
+  }
+}
+`,
+		},
+
+		{
+			Description: "ExecRequest YAML",
+			URL:         "test/req/exec.yaml",
+			Provider: func(URL string) interface{} {
+				req, _ := NewExecRequestFromURL(URL)
+				return req
+			},
+			Expected: `{
+  "Target": {
+    "URL": "scp://127.0.0.1:22/",
+    "Credential": "/var/folders/gl/5550g3kj6tn1rbz8chqx1c61ycmmm1/T/dummy20.json"
+  },
+  "Name": "testMysql",
+  "Command": "mysqldump  -uroot -p***mysql*** --all-databases --routines | grep -v 'Warning' \u003e /tmp/dump.sql",
+  "Secrets": {
+    "***mysql***": "/var/folders/gl/5550g3kj6tn1rbz8chqx1c61ycmmm1/T/mysql20.json"
+  },
+  "Interactive": true,
+  "AllocateTerminal": true,
+  "RunInTheBackground": false
+}
+`,
+		},
+	}
+
+	for _, useCase := range useCases {
+		request := useCase.Provider(useCase.URL)
+		assertly.AssertValues(t, useCase.Expected, request, useCase.Description)
+	}
+}
+
+func TestContainerBaseRequest_Validate(t *testing.T) {
+	{
+		var stopRequest = &StopRequest{BaseRequest: &BaseRequest{}}
+		assert.NotNil(t, stopRequest.Validate())
+	}
+	{
+		var stopRequest = &StopRequest{BaseRequest: &BaseRequest{Target: url.NewResource("abc")}}
+		assert.NotNil(t, stopRequest.Validate())
+	}
+}
+
+func TestRunRequest_Validate(t *testing.T) {
+	{
+		var stopRequest = &RunRequest{}
+		assert.NotNil(t, stopRequest.Validate())
+	}
+	{
+		var stopRequest = &RunRequest{Target: url.NewResource("abc")}
+		assert.NotNil(t, stopRequest.Validate())
+	}
+}
---
+++ system/storage/service_contract_test.go
@@ -0,0 +1,204 @@
+package storage
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/viant/assertly"
+	"github.com/viant/toolbox/url"
+	"testing"
+)
+
+func TestNewCopyRequestFromURL(t *testing.T) {
+
+	var useCases = []struct {
+		Description     string
+		URL             string
+		ExpectedRequest interface{}
+		HasError        bool
+	}{
+		{
+			Description: "yaml asset request",
+			URL:         "test/copy1.yaml",
+			ExpectedRequest: `{
+  "Source": {
+    "URL": "mem://yaml1/dir"
+  },
+  "Dest": {
+    "URL": "mem://dest/dir2"
+  },
+  "Assets": {
+    "file1.txt": null,
+    "file2.txt": "renamedFile2"
+  },
+  "Transfers": [
+    {
+      "Source": {
+        "URL": "mem://yaml1/dir/file1.txt"
+      },
+      "Dest": {
+        "URL": "mem://dest/dir2/file1.txt"
+      }
+    },
+    {
+      "Source": {
+        "URL": "mem://yaml1/dir/file2.txt"
+      },
+      "Dest": {
+        "URL": "mem://dest/dir2/renamedFile2"
+      }
+    }
+  ]
+}`,
+		},
+		{
+			Description: "json root source and dest only",
+			URL:         "test/copy2.json",
+			ExpectedRequest: `{
+  "Source": {
+    "URL": "mem://yaml1/dir"
+  },
+  "Dest": {
+    "URL": "mem://dest/dir2"
+  },
+  "Compress": true,
+  "Expand": true,
+  "Replace": {
+    "k1": "1",
+    "k2": "2"
+  },
+  "Transfers": [
+    {
+      "Source": {
+        "URL": "mem://yaml1/dir"
+      },
+      "Dest": {
+        "URL": "mem://dest/dir2"
+      }
+    }
+  ]
+}`,
+		},
+
+		{
+			Description: "yaml transfers request",
+			URL:         "test/copy3.yaml",
+			ExpectedRequest: `{
+  "Transfers": [
+    {
+      "Expand": true,
+      "Source": {
+        "URL": "file1.txt"
+      },
+      "Dest": {
+        "URL": "file101.txt"
+      }
+    },
+    {
+      "Expand": true,
+      "Source": {
+        "URL": "file2.txt"
+      },
+      "Dest": {
+        "URL": "file201.txt"
+      }
+    }
+  ]
+}`,
+		},
+
+		{
+			Description: "yaml transfers relative path update",
+			URL:         "test/copy4.json",
+			ExpectedRequest: `{
+  "Source": {
+    "URL": "mem://yaml1/dir"
+  },
+  "Dest": {
+    "URL": "mem://dest/dir2"
+  },
+  "Transfers": [
+    {
+      "Source": {
+        "URL": "mem://yaml1/dir/file1.txt"
+      },
+      "Dest": {
+        "URL": "mem://dest/file1.txt"
+      }
+    }
+  ]
+}
+`,
+		},
+		{
+			Description: "yaml asset relative path update",
+			URL:         "test/copy5.yaml",
+			ExpectedRequest: `{
+  "Source": {
+    "URL": "scp://127.0.0.1:7722/echo",
+    "Credential": "mem://github.com/viant/endly/workflow/docker/build/secret/build.json"
+  },
+  "Dest": {
+    "URL": "ssh://127.0.0.1/",
+    "Credential": "/Users/awitas/.secret/localhost.json"
+  },
+  "Assets": {
+    "/echo": "/tmp/echo/"
+  },
+  "Transfers": [
+    {
+      "Source": {
+        "URL": "scp://127.0.0.1:7722/echo",
+        "Credential": "mem://github.com/viant/endly/workflow/docker/build/secret/build.json"
+      },
+      "Dest": {
+        "URL": "ssh://127.0.0.1/tmp/echo/",
+        "Credential": "/Users/awitas/.secret/localhost.json"
+      }
+    }
+  ]
+}
+`,
+		},
+	}
+
+	for _, useCase := range useCases {
+		request, err := NewCopyRequestFromuRL(useCase.URL)
+		if useCase.HasError {
+			assert.NotNil(t, err, useCase.Description)
+			continue
+		}
+		if !assert.Nil(t, err, useCase.Description) {
+			continue
+		}
+
+		assert.Nil(t, request.Init(), useCase.Description)
+		assert.Nil(t, request.Validate(), useCase.Description)
+		assertly.AssertValues(t, useCase.ExpectedRequest, request, useCase.Description)
+	}
+
+}
+
+func TestCopyRequest_Validate(t *testing.T) {
+
+	{
+		var request = NewCopyRequest(nil)
+		assert.NotNil(t, request.Validate())
+	}
+	{
+		var request = NewCopyRequest(nil, NewTransfer(nil, url.NewResource("abc"), false, false, nil))
+		assert.NotNil(t, request.Validate())
+	}
+	{
+		var request = NewCopyRequest(nil, NewTransfer(url.NewResource("abc"), nil, false, false, nil))
+		request.Init()
+		assert.NotNil(t, request.Validate())
+	}
+	{
+		var request = NewCopyRequest(nil, NewTransfer(url.NewResource("abc"), url.NewResource("abc"), false, false, nil))
+		assert.NotNil(t, request.Validate())
+	}
+	{
+		var request = NewCopyRequest(nil, NewTransfer(url.NewResource("abc"), url.NewResource("abc"), false, false, nil))
+		request.Init()
+		assert.Nil(t, request.Validate())
+	}
+}
