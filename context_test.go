679ef7e19a6ab4f77f38ea1e55e6f8359c4bceec
adrianwit <adrianwit@gmail.com>
2018-04-02 11:36:27 -0700

    updated test

--- context_test.go
+++ context_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/viant/toolbox/url"
 	"strings"
 	"testing"
+	"sync/atomic"
 )
 
 func TestNewDefaultState(t *testing.T) {
@@ -31,5 +32,77 @@ func TestContext_Expand_Resource(t *testing.T) {
 	assert.NotNil(t, err)
 	_, err = context.ExpandResource(&url.Resource{})
 	assert.NotNil(t, err)
+
+	{
+		state := context.State()
+		state.Put("host", "127.0.0.1")
+		expanded, err := context.ExpandResource(url.NewResource("scp://${host}/as"))
+		if assert.Nil(t, err) {
+			assert.EqualValues(t, "scp://127.0.0.1/as", expanded.URL)
+		}
+	}
+	{
+		_, err := context.ExpandResource(url.NewResource("path"))
+		assert.Nil(t, err)
+	}
+
+}
+
+func TestContext_NewRequest(t *testing.T) {
+	manager := endly.New()
+	context := manager.NewContext(toolbox.NewContext())
+	{
+		request, err := context.NewRequest("nop", "nop")
+		if assert.Nil(t, err) {
+			assert.NotNil(t, request)
+		}
+	}
+	{
+		_, err := context.NewRequest("invalid", "nop")
+		assert.NotNil(t, err)
+	}
+	{
+		_, err := context.NewRequest("nop", "abc")
+		assert.NotNil(t, err)
+	}
+
+}
+
+func TestContext_AsRequest(t *testing.T) {
+	manager := endly.New()
+	context := manager.NewContext(toolbox.NewContext())
+	{
+		request, err := context.AsRequest("nop", "nop", map[string]interface{}{})
+		if assert.Nil(t, err) {
+			assert.NotNil(t, request)
+		}
+	}
+	{
+		_, err := context.AsRequest("zzz", "nop", map[string]interface{}{})
+		assert.NotNil(t, err)
+	}
+
+}
+
+func TestContext_Clone(t *testing.T) {
+	manager := endly.New()
+	{
+		context := manager.NewContext(toolbox.NewContext())
+		cloned := context.Clone()
+		assert.NotNil(t, cloned)
+
+	}
+	{
+		var deferredRun int32 = 0
+		context := manager.NewContext(toolbox.NewContext())
+		context.Deffer(func() {
+			atomic.AddInt32(&deferredRun, 1)
+		})
+		cloned := context.Clone()
+		assert.False(t, cloned.IsClosed())
+		context.Close()
+		assert.True(t, cloned.IsClosed())
+		assert.EqualValues(t, int32(2), deferredRun)
+	}
 
 }
