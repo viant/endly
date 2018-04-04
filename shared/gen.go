cad24fced2e73ee6bb86f1fffb4b7e82439fe29a
adrianwit <adrianwit@gmail.com>
2018-04-04 10:12:22 -0700

    moved gen to shared

---
+++ shared/gen.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/viant/endly"
+	"github.com/viant/toolbox"
+	"github.com/viant/toolbox/storage"
+	"log"
+	"path"
+
+	"strings"
+)
+
+//main generate file with static content from meta, workflow, req folders so that they can be compiled into final binary
+func main() {
+	callerDir := toolbox.CallerDirectory(3)
+	parent := strings.Replace(callerDir, "/shared/", "", 1)
+
+
+	mappings := []*storage.StorageMapping{
+		{
+			SourceURL:      toolbox.FileSchema + path.Join(parent, "shared/meta"),
+			DestinationURI: path.Join(endly.Namespace, "meta"),
+			TargetFile:     path.Join(parent, "static", "meta.go"),
+			TargetPackage:  "static",
+		},
+		{
+			SourceURL:      toolbox.FileSchema + path.Join(parent, "shared/workflow"),
+			DestinationURI: path.Join(endly.Namespace, "workflow"),
+			TargetFile:     path.Join(parent, "static", "workflow.go"),
+			TargetPackage:  "static",
+		},
+		{
+			SourceURL:      toolbox.FileSchema + path.Join(parent, "shared/req"),
+			DestinationURI: path.Join(endly.Namespace, "req"),
+			TargetFile:     path.Join(parent, "static", "req.go"),
+			TargetPackage:  "static",
+		},
+		{
+			SourceURL:      toolbox.FileSchema + path.Join(parent, "Version"),
+			DestinationURI: path.Join(endly.Namespace, "Version"),
+			TargetFile:     path.Join(parent, "static", "version.go"),
+			TargetPackage:  "static",
+		},
+	}
+	err := storage.GenerateStorageCode(mappings...)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
