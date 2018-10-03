4432e66d5e4ea0e87d5602316085c84676aab676
adrianwit <adrianwit@gmail.com>
2018-10-02 20:35:13 -0700

    updated shared code

--- static/version.go
+++ static/version.go
@@ -7,9 +7,9 @@ import (
 )
 
 func init() {
-	var memStorage = storage.NewMemoryService();
+	var memStorage = storage.NewMemoryService()
 	{
-		err := memStorage.Upload("mem://github.com/viant/endly/Version", bytes.NewReader([]byte{48,46,49,50,46,50,10}))
+		err := memStorage.Upload("mem://github.com/viant/endly/Version", bytes.NewReader([]byte{48, 46, 49, 50, 46, 51, 10}))
 		if err != nil {
 			log.Printf("failed to upload: mem://github.com/viant/endly/Version %v", err)
 		}
