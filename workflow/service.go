f0163768a0846cb481c27c3cc28a0b11796d1283
adrianwit <adrianwit@gmail.com>
2018-11-26 23:29:57 -0800

    added current tag id to workflow state

--- workflow/service.go
+++ workflow/service.go
@@ -109,6 +109,7 @@ func (s *Service) runAction(context *endly.Context, action *model.Action, proces
 	var request interface{}
 	process.Push(activity)
 	startEvent := s.Begin(context, activity)
+	state.Put("tagId", action.TagID)
 	err = s.runNode(context, "action", process, action.AbstractNode, func(context *endly.Context, process *model.Process) (in, out data.Map, err error) {
 		defer s.End(context)(startEvent, model.NewActivityEndEvent(activity))
 		defer process.Pop()
