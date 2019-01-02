875e7af95717aef8805d29a966654f044f24c9ca
adrianwit <adrianwit@gmail.com>
2019-01-01 22:24:24 -0800

    moved statis pkg

---
+++ shared/static/version.go
@@ -0,0 +1,17 @@
+package static
+
+import (
+	"bytes"
+	"github.com/viant/toolbox/storage"
+	"log"
+)
+
+func init() {
+	var memStorage = storage.NewMemoryService()
+	{
+		err := memStorage.Upload("mem://github.com/viant/endly/Version", bytes.NewReader([]byte{48, 46, 50, 52, 46, 49, 10}))
+		if err != nil {
+			log.Printf("failed to upload: mem://github.com/viant/endly/Version %v", err)
+		}
+	}
+}
