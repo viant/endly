759fb109ce57c87b00a2aac8f824bcfc4c22f42f
adrianwit <adrianwit@gmail.com>
2019-01-22 21:48:53 -0800

    added generic repeated message

---
+++ model/msg/repeated.go
@@ -0,0 +1,18 @@
+package msg
+
+//RepeatedEvent represents a generic repeated message
+type RepeatedEvent struct {
+	tag string
+	message string
+}
+
+func (e *RepeatedEvent) Message(repeated *Repeated) *Message {
+	var tag = NewStyled(e.tag, MessageStyleGeneric)
+	var title  = NewStyled(e.message, MessageStyleGeneric)
+	return NewMessage(title, tag)
+}
+
+//NewSleepEvent create a new sleep event
+func NewRepeatedEvent(message, tag string) *RepeatedEvent {
+	return &RepeatedEvent{message:message, tag:tag}
+}
