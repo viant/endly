61ba1d5e5d875242487fcc2bd8caeba21ac81fad
adrianwit <adrianwit@gmail.com>
2019-01-25 08:52:24 -0800

    patched upload

--- system/cloud/gc/upload.go
+++ system/cloud/gc/upload.go
@@ -1,45 +1,35 @@
 package gc
 
 import (
+	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
-
-
-func Upload(httpClient *http.Client, signedURL string, reader io.Reader) error {
-	request, err:= http.NewRequest("POST", signedURL, strings.NewReader(""))
+func Upload(httpClient *http.Client, uploadURL string, reader io.Reader) error {
+	data, err := ioutil.ReadAll(reader)
 	if err != nil {
 		return err
 	}
-	request.Header.Set("x-goog-content-type", "application/zip")
+	request, err := http.NewRequest("PUT", uploadURL, bytes.NewReader(data))
+	if err != nil {
+		return err
+	}
 	request.Header.Set("content-type", "application/zip")
-	request.Header.Set("x-goog-resumable", "start")
+	request.Header.Set("x-goog-content-length-range", "0,104857600")
+	request.Header.Set("Content-Length", fmt.Sprintf("%d", len(data)))
 	response, err := httpClient.Do(request)
 	if err != nil {
 		return err
 	}
-	if response.StatusCode != http.StatusCreated {
-		return fmt.Errorf("failed to init upload code: %v",  response.Status)
+	var message []byte
+	if response.ContentLength > 0 {
+		message, err = ioutil.ReadAll(response.Body)
 	}
-
-	 putRequest, err := http.NewRequest("PUT", signedURL, reader)
-	 if err != nil {
-	 	return err
-	 }
-
-
-	putRequest.Header.Set("content-type", "application/zip")
-	putRequest.Header.Set("x-goog-content-type", "application/zip")
-	putRequest.Header.Set("x-goog-content-length-range", "0,104857600")
-	response, err = httpClient.Do(putRequest)
-	 if err != nil {
-	 	return err
-	 }
-	if response.StatusCode != http.StatusOK {
-		return fmt.Errorf("failed to init upload code: %v", response.Status)
+	if response.StatusCode/100 != 2 {
+		return fmt.Errorf("failed to upload code: %v, %s", response.Status, message)
 	}
 	return nil
 }
