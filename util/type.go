107659e724a33a3138801f9de217bc9fc5db07fa
adrianwit <adrianwit@gmail.com>
2019-02-05 16:42:53 -0800

    added SimpleTypeName util method

---
+++ util/type.go
@@ -0,0 +1,11 @@
+package util
+
+import "strings"
+
+//SimpleTypeName returns simple type name
+func SimpleTypeName(typeName string) string {
+	if index := strings.Index(typeName, "."); index != -1 {
+		return string(typeName[index+1:])
+	}
+	return typeName
+}
