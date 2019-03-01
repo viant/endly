2b84332d2b98ccdb76d04d30d5784212b2a9fe51
adrianwit <adrianwit@gmail.com>
2019-03-01 11:58:38 -0800

    added Kubernetes auth plugin package

--- system/kubernetes/shared/client.go
+++ system/kubernetes/shared/client.go
@@ -9,6 +9,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"os"
 	"path/filepath"
 	"strings"
