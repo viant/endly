989a906cc07e52224e56fb009feee1b632b053c1
adrianwit <adrianwit@gmail.com>
2019-07-20 16:32:18 -0700

    reformatted

--- model/task.go
+++ model/task.go
@@ -5,7 +5,7 @@ type Task struct {
 	*AbstractNode
 	Actions []*Action //actions
 	*TasksNode
-	Fail    bool //controls if return fail status workflow on catch task
+	Fail bool //controls if return fail status workflow on catch task
 
 	//internal only for inline workflow meta data
 
